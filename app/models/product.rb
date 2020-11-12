8739c64d66185ad422f04f95214560e096f1e280
brian <riceb53@gmail.com>
2020-11-11 19:49:40 -0500

    add more model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,18 @@ class Product < ApplicationRecord
   def tax
     price * 0.09
   end
+
+  def is_discounted?
+    price < 10
+    # if price < 10
+    #   return true
+    # else
+    #   return false
+    # end
+  end
+
+  def total
+    price + tax
+    # price + (price * 0.09)
+  end
 end
