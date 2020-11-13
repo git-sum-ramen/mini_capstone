3c99de0d071eb626f5c1a9112b4752291ecc5e76
brian <riceb53@gmail.com>
2020-11-12 19:18:04 -0500

    add migrations

---
+++ db/migrate/20201113001112_make_changes_to_products.rb
@@ -0,0 +1,7 @@
+class MakeChangesToProducts < ActiveRecord::Migration[6.0]
+  def change
+    change_column :products, :description, :text
+    change_column :products, :price, :decimal, precision: 8, scale: 2
+    add_column :products, :inventory, :integer
+  end
+end
