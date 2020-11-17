7063be6ed5d4defca7fcec3abf6a206b3724282c
brian <riceb53@gmail.com>
2020-11-16 19:45:08 -0500

    add activerecord sorting/filtering

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,26 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    # @products = Product.all
+    if params[:search]
+      @products = Product.where("name ILIKE ?", "%#{params[:search]}%")
+    else
+      @products = Product.all
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "asc"
+      @products = @products.order(price: :asc)
+    elsif params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(price: :desc)
+    else
+      @products = @products.order(id: :asc)
+    end
+
+    if params[:discount] == "true"
+      @products = @products.where("price < 20")
+    end
+
+    
+    
     render "index.json.jb"
   end
 
