341675dceaec5fad19e11b746acfd03959d262f7
brian <riceb53@gmail.com>
2020-11-17 19:30:35 -0500

    rewrite associations with has_many and belongs_to

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ApplicationRecord
   validates :name, :price, :description, presence: true
   validates :description, length: { in: 10..500 }
+  belongs_to :supplier
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
   
   
   def tax
