4b5e7ede4048d8b8c2b60d6a8850df299e1b3c44
brian <riceb53@gmail.com>
2020-11-19 21:53:22 -0500

    add orders show, index, add product data to show and index views

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,4 +1,10 @@
 class Api::OrdersController < ApplicationController
+  def index
+    # @orders = Order.where(user_id: current_user.id)
+    @orders = current_user.orders
+    render 'index.json.jb'
+  end
+  
   def create
     product = Product.find_by(id: params[:product_id])
     
@@ -15,7 +21,14 @@ class Api::OrdersController < ApplicationController
       tax: calculated_tax,
       total: calculated_total
     )
+
     @order.save
+    render 'show.json.jb'
+  end
+
+  def show
+    # @order = Order.find_by(id: params[:id])
+    @order = current_user.orders.find_by(id: params[:id])
     render 'show.json.jb'
   end
 end
